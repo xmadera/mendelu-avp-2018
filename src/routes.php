0bd8375a51548b46fd38e8033d93c907cc36352b
Jiří Lýsek <jiri.lysek@mendelu.cz>
2017-08-02 11:12:48 +0200

    simple form, log and templates

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,19 @@
 <?php
-// Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
+$app->get('/[{name}]', function (Request $request, Response $response, $args) {
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+    return $this->view->render($response, 'index.latte');
+})->setName('index');
+
+$app->post('/test', function (Request $request, Response $response, $args) {
+    //read POST data
+    $input = $request->getParsedBody();
+
+    //log
+    $this->logger->info('Your name: ' . $input['person']);
+
+    return $response->withHeader('Location', $this->router->pathFor('index'));
+})->setName('redir');
